afa7a0f8f4c006dad9e0d2062e1919d00177af1d
Leanbader <baderleandro@gmail.com>
2023-11-21 00:56:54 -0300

    Se completo hasta el punto 10 de los ejercicios

---
+++ js/Ejercicio8.js
@@ -0,0 +1,17 @@
+/*8.- Escribe un programa que pida un número y diga si es divisible por 2
+Ejemplo:  
+input: 10
+input: 15
+Output: El 10 es divisible por 2.
+Output: El 15 no es divisible por 2. 
+*/
+
+let numero;
+
+numero = parseInt(prompt("Ingrese un numero"));
+
+if (numero%2 == 0) {
+    document.write("El ", numero, " es divisible por 2");
+} else {
+    document.write("El ", numero, " no es divisible por 2");
+}
